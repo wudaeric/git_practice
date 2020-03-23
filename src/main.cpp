dee266e2d18764cc8b5825267adddddc02b5b8d8
DaWu <dawu@system76-pc.localdomain>
2020-03-23 15:28:00 -0700

    Episode #2 - Print number

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <vector>
+
 
 int main()
 {
     std::cout << "hello git" << std::endl;
     std::cout << "One more change\n";
+
+    std::vector<int> number;
+    for(int i = 0; i < 4; i++)
+    {
+        number.emplace_back(i);
+    }
+
+    for(auto& n : number)
+    {
+        std::cout << n << std::endl;
+    }
 }
